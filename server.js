ac95717356406f567206c835af14aeee80d04147
unknown <shishirdhasmana@gmail.com>
2026-04-05 22:00:40 +0530

    Complete assignment

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const app = require("./src/app");
-
 const PORT = process.env.PORT || 5000;
+const BASE_URL = process.env.RENDER_EXTERNAL_URL || `http://localhost:${PORT}`;
 
 app.listen(PORT, () => {
   console.log(`
-      Server:    http://localhost:${PORT}  
-      API Docs:  http://localhost:${PORT}/api-docs
+      Server:    ${BASE_URL}
+      API Docs:  ${BASE_URL}/api-docs
   `);
 });
